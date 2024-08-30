c69db3e492b7c3e956b796d1422f5493032ab76d
Jan Beukes <28087887@sun.ac.za>
2024-08-30 17:46:22 +0200

    refactored to be usable with opencv can import to convert opencsv images/frames to ascii characters

--- ascii_art.py
+++ ascii_art.py
@@ -1,54 +1,60 @@
-import picture
+import cv2
 import sys
+import os
 from color import Color
 
 ## CONSTANTS
-darkness_table = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
-downscale = 0.1
+darkness_table = " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+#darkness_table = " .',:;xlxokXdO0KN"
 GRAYSCALE_FACTOR =  (0.299,0.587,0.114)
-ASCII_FACTOR = (len(darkness_table)-1)/255
-SQUISH_FACTOR = 0.6
+ASCII_FACTOR = (len(darkness_table)-1)/255 # factor to get ascii character from brightness
+SQUISH_FACTOR = 0.6 # decrease height to better match original
 
-## Get input
-try:
-    image = picture.Picture(sys.argv[1])
-    if len(sys.argv) > 2:
-        if sys.argv[2] == '-s':
-            downscale = float(sys.argv[3])
-        else:
-            raise IndexError
-            
-    out_mode = input('Output [t] for terminal [f] for file: ')
-    if out_mode.lower() not in ['t','f']:
-        exit()
-except FileNotFoundError:
-    print(f"image {sys.argv[1]} not found")
-    exit()
-except IndexError:
-    print("Invalid args")
-    exit()
-   
-# Create ascii array   
-art_height, art_width = int(downscale*image.height()), int(downscale*image.width())
-art = [""]*art_height
-image2 = picture.Picture(art_width,art_height)
-
-#loop through art array and select ascii from relative pixel
-for y in range(art_height):
-    for x in range(art_width):
-        c = image.get(int((1/downscale)*x),int((1/downscale)*y))
-        grey = c.getRed()*GRAYSCALE_FACTOR[0] + c.getGreen()*GRAYSCALE_FACTOR[1] + c.getBlue()*GRAYSCALE_FACTOR[2]
-        grey = int(grey/3)
-        c = Color(grey,grey,grey)
-        image2.set(x,y,c)
-        char = darkness_table[int((255-grey) * ASCII_FACTOR)]
-        art[y] += char
-    art[y] += "\n"
-image2.save("out.png")        
-
-with open("out.txt","w") as file:
+# Create ascii array 
+def get_art(image, downscale):
+    width,height = image.shape[0],image.shape[1]
+    art_width, art_height = int(downscale*height), int(downscale*width)
+    art = [""]*art_height
     
-    squish = int(art_height * SQUISH_FACTOR)
-    for row in range(squish):
-        if out_mode.lower() == 'f': file.write(art[int(row/SQUISH_FACTOR)])
-        if out_mode.lower() == 't': print(art[int(row/SQUISH_FACTOR)],end="")
+    #loop through art array and select ascii from relative pixel
+    for y in range(art_height):
+        for x in range(art_width):
+            c = image[int((1/downscale)*y)][int((1/downscale)*x)]
+            grey = c[0]*GRAYSCALE_FACTOR[0] + c[1]*GRAYSCALE_FACTOR[1] + c[2]*GRAYSCALE_FACTOR[2]
+            char = darkness_table[int(grey * ASCII_FACTOR)]
+            art[y] += char
+    return art       
+
+def output_art(art, out_mode):
+    art_height = len(art)
+    with open("out.txt","w") as file: 
+        squish = int(art_height * SQUISH_FACTOR)
+        os.system("clear")
+        if out_mode == 't': print('\n'.join((''.join(art[int(row/SQUISH_FACTOR)]) for row in range(squish))), end='')
+        if out_mode == 'f': file.write('\n'.join((''.join(row) for row in art)), end='')
+def main():
+    downscale = 0.1
+    # Get input
+    try:
+        image = cv2.imread((sys.argv[1]))
+        if len(sys.argv) > 2:
+            if sys.argv[2] == '-s':
+                downscale = float(sys.argv[3])
+            else:
+                raise IndexError
+                
+        out_mode = input('Output [t] for terminal [f] for file: ')
+        if out_mode.lower() not in ['t','f']:
+            exit()
+    except FileNotFoundError:
+        print(f"image {sys.argv[1]} not found")
+        exit()
+    except IndexError:
+        print("Invalid args")
+        exit()
+        
+    art = get_art(image, downscale)
+    output_art(art, out_mode)
+    
+if __name__ == "__main__": main()
+    
--- opencv_test.py
+++ opencv_test.py
@@ -1,14 +1,26 @@
+import sys
 import cv2
-import os
+import ascii_art
 
+downscale = 0.2
+
+if len(sys.argv) > 1:
+    try:
+        downscale = float(sys.argv[1])
+    except TypeError:
+        print("invalid args")
+        
 vid = cv2.VideoCapture(0)
 
 while True:
-    
+
     ret, frame = vid.read()
     
+    art = ascii_art.get_art(frame, downscale)
+    ascii_art.output_art(art, 't')
+    
     cv2.imwrite('hello.png',frame)
-    cv2.imshow('frame',frame)
+    #cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
          break
     
